c168581ed2cbe02f41dc88a5bb2fa8b33d0318d8
Phieo <shikong2014@gmail.com>
2018-05-30 16:39:16 +0800

    refactor: template structure

---
+++ template/pages/home/index.js
@@ -0,0 +1,6 @@
+Page({
+  data: {
+    thumb: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+    footerImg: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+  },
+});
